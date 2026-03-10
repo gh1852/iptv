1c505f5628ac1582932ea95beaf160dbae98e9b6
jons <gh@szyihecloud.com>
2026-03-10 18:02:23 +0800

    fix(tv): 优化播放稳定性

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jons.iptv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
