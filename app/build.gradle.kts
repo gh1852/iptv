1de9453fff655cf53eb6f70c342f69aa75311c55
jons <jons@jons.com>
2026-03-11 22:23:57 +0800

    feat(ui): add loading indicator when app starts loading channels

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jons.iptv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
